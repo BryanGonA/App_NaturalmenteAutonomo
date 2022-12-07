31642908e428060a0b5f6c77946fd65cee2c7aca
Bryan Gonzalez <72216201+BryanGonA@users.noreply.github.com>
2022-12-07 10:15:25 -0500

    implementacion btns en el HomeScreen

--- src/screens/components/LoginForm.jsx
+++ src/screens/components/LoginForm.jsx
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 8,
-    marginTop: 20,
+    marginTop: 10,
   },
   textError: {
     color: 'white',
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,13 +1,51 @@
 import React, { Component, useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function HomeScreen({ navigation }) {
 return (
     
     <View style={styles.container}>
+        <ImageBackground>
+            <Image
+            source={require('../assets/log_blanck.png')}
+            style={{ width: 200, height: 200, marginTop: 0, margin: 30}}
+            resizeMode="contain"
+            />
+        </ImageBackground>
 
-        <Text style={styles.title}>Naturalmente</Text>
-        <Text style={styles.title}>Autónomo</Text>
+        <TouchableOpacity >
+            <Image
+            source={require('../assets/btn_nutri.png')}
+            style={{ width: 450, height: 80, margin:10 ,alignItems:"center",justifyContent:"center" }}
+            resizeMode="contain"
+            />
+            
+        </TouchableOpacity>
+        <TouchableOpacity >
+            <Image
+            source={require('../assets/btn_vida.png')}
+            style={{ width: 450, height: 80, margin:10 ,alignItems:"center",justifyContent:"center" }}
+            resizeMode="contain"
+            />
+            
+        </TouchableOpacity>
+        <TouchableOpacity >
+            <Image
+            source={require('../assets/btn_aliments.png')}
+            style={{ width: 450, height: 80, margin:10 ,alignItems:"center",justifyContent:"center" }}
+            resizeMode="contain"
+            />
+            
+        </TouchableOpacity>
+        <TouchableOpacity >
+            <Image
+            source={require('../assets/btn_fisic.png')}
+            style={{ width: 450, height: 80, margin:10 ,alignItems:"center",justifyContent:"center" }}
+            resizeMode="contain"
+            />
+            
+        </TouchableOpacity>
         
     </View>
     
