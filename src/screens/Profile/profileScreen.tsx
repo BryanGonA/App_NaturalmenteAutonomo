a0f0716661f2970469fd21b875e0c6015f9d2946
Bryan Gonzalez <72216201+BryanGonA@users.noreply.github.com>
2023-08-15 15:51:38 -0500

    Added ProfileScreen

--- src/screens/Profile/profileScreen.tsx
+++ src/screens/Profile/profileScreen.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
-import ImagePicker from 'react-native-image-picker';
+import { Ionicons, MaterialIcons, Feather } from "@expo/vector-icons";
 
 import styles from './profileStyles'
 
@@ -23,12 +23,17 @@ interface User {
 
 const ProfileScreen: React.FC<Props> = ({ name, email, avatar }) => {
   const navigation = useNavigation();
-  const [user, setUser] = useState<User>({ id: 0, name: 'Bryan Gonzalez', email: 'bgonzalez@uao.edu.co', avatar: '', tokens: 0 });
-  const [newAvatar, setNewAvatar] = useState('');
+  const [user, setUser] = useState<User>({
+    id: 0,
+    name: "Bryan Gonzalez",
+    email: "bgonzalez@uao.edu.co",
+    avatar: "",
+    tokens: 0,
+  });
+  const [newAvatar, setNewAvatar] = useState("");
 
   useEffect(() => {
     // Obtiene la información del usuario de la base de datos
-    
     /*axios.get(`https://api.example.com/users/${userId}`).then((response) => {
       setUser(response.data);
     });*/
@@ -72,24 +77,116 @@ const ProfileScreen: React.FC<Props> = ({ name, email, avatar }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-        <Icon name="chevron-left" size={25} color="#555" style={styles.backButtonIcon} />
-      </TouchableOpacity>
-      <View style={styles.profileContainer}>
-        <TouchableOpacity onPress={handleSelectAvatar}>
-          <Image source={{ uri: newAvatar || avatar }} style={styles.avatar} />
-        </TouchableOpacity>
-        <Text style={styles.name}>{user.name}</Text>
-        <Text style={styles.email}>{user.email}</Text>
-        <Text style={styles.tokens}>Puntos: {user.tokens}</Text>
-        {newAvatar ? (
-          <TouchableOpacity onPress={handleSaveAvatar} style={styles.saveButton}>
-            <Text style={styles.saveButtonText}>Guardar</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.titleBar}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionicons name="ios-arrow-back" size={30} color="#52575D" />
           </TouchableOpacity>
-        ) : null}
-      </View>
-    </View>
+          <Feather name="more-vertical" size={30} color="#52575D" />
+        </View>
+
+        <View style={{ alignSelf: "center" }}>
+          <View style={styles.profileImage}>
+            <Image
+              source={require("../../assets/photo.jpg")}
+              style={styles.image}
+              resizeMode="center"
+            ></Image>
+          </View>
+          {/*
+          <View style={styles.dm}>
+            <MaterialIcons
+              name="chat"
+              size={15}
+              color="#DFD8C8"
+            ></MaterialIcons>
+          </View>*/}
+          
+          <View style={styles.active}></View>
+          
+          <View style={styles.add}>
+          <TouchableOpacity onPress={handleSelectAvatar}>
+              <Ionicons
+                name="ios-add"
+                size={25}
+                color="#DFD8C8"
+                style={{ marginTop: 1, marginLeft: 2 }}
+              ></Ionicons>
+          </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>
+            Alejandra 
+          </Text>
+          <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>
+            Estudiante
+          </Text>
+        </View>
+
+        <View style={styles.statsContainer}>
+          <View style={styles.statsBox}>
+            <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
+            <Text style={[styles.text, styles.subText]}>Puntos</Text>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 32 }}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("../../assets/photo.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("../../assets/profile.png")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("../../assets/photo.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={[styles.subText, styles.recent]}>Actividad reciente</Text>
+        <View style={{ alignItems: "center" }}>
+          <View style={styles.recentItem}>
+            <View style={styles.activityIndicator}></View>
+            <View style={{ width: 250 }}>
+              <Text
+                style={[styles.text, { color: "#41444B", fontWeight: "300" }]}
+              >
+                Te inscribiste al evento{" "}
+                <Text style={{ fontWeight: "400" }}>Recetas saludables</Text> hace{" "}
+                <Text style={{ fontWeight: "400" }}>2 minutos.</Text>
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.recentItem}>
+            <View style={styles.activityIndicator}></View>
+            <View style={{ width: 250 }}>
+              <Text
+                style={[styles.text, { color: "#41444B", fontWeight: "300" }]}
+              >
+                Haz ganado 15 puntos por participar en{" "}
+                <Text style={{ fontWeight: "400" }}>Caminantes UAO</Text>
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Profile/profileStyles.tsx
+++ src/screens/Profile/profileStyles.tsx
@@ -3,56 +3,126 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
       flex: 1,
-      backgroundColor: '#f1f1f1',
-    },
-    backButton: {
-      position: 'absolute',
-      top: 10,
-      left: 10,
-      zIndex: 1,
-    },
-    backButtonIcon: {
-      marginRight: 10,
-    },
-    profileContainer: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 50,
-    },
-    avatar: {
-      width: 150,
-      height: 150,
-      borderRadius: 75,
-      marginBottom: 20,
-    },
-    name: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-    email: {
-      fontSize: 18,
-      color: '#666',
-      marginBottom: 30,
-    },
-    tokens: {
-      fontSize: 18,
-      color: '#666',
-      marginBottom: 10,
-    },
-    saveButton: {
-      backgroundColor: '#555',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderRadius: 5,
+      backgroundColor: "#FFF",
+  },
+  text: {
+      color: "#52575D"
+  },
+  image: {
+      flex: 1,
+      height: undefined,
+      width: undefined,
+      resizeMode: "contain"
+  },
+  titleBar: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 38,
+      marginHorizontal: 16
+  },
+  subText: {
+      fontSize: 12,
+      color: "#AEB5BC",
+      textTransform: "uppercase",
+      fontWeight: "500"
+  },
+  profileImage: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      overflow: "hidden",
       marginTop: 20,
-    },
-    saveButtonText: {
-      color: '#fff',
-      fontWeight: 'bold',
-    },
-  });
+  },
+  dm: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      top: 20,
+      width: 30,
+      height: 30,
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center"
+  },
+  active: {
+      backgroundColor: "#34FFB9",
+      position: "absolute",
+      bottom: 28,
+      left: 10,
+      padding: 4,
+      height: 20,
+      width: 20,
+      borderRadius: 10
+  },
+  add: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      bottom: 10,
+      right: 10,
+      width: 30,
+      height: 30,
+      borderRadius: 30,
+      alignItems: "center",
+      justifyContent: "center",
+  },
+  infoContainer: {
+      alignSelf: "center",
+      alignItems: "center",
+      marginTop: 16
+  },
+  statsContainer: {
+      flexDirection: "row",
+      alignSelf: "center",
+      marginTop: 32
+  },
+  statsBox: {
+      alignItems: "center",
+      flex: 1
+  },
+  mediaImageContainer: {
+      width: 180,
+      height: 200,
+      borderRadius: 12,
+      overflow: "hidden",
+      marginHorizontal: 10
+  },
+  mediaCount: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      top: "50%",
+      marginTop: -50,
+      marginLeft: 30,
+      width: 100,
+      height: 100,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 12,
+      shadowColor: "rgba(0, 0, 0, 0.38)",
+      shadowOffset: { width: 0, height: 10 },
+      shadowRadius: 20,
+      shadowOpacity: 1
+  },
+  recent: {
+      marginLeft: 78,
+      marginTop: 32,
+      marginBottom: 6,
+      fontSize: 10
+  },
+  recentItem: {
+      flexDirection: "row",
+      alignItems: "flex-start",
+      marginBottom: 16
+  },
+  activityIndicator: {
+      backgroundColor: "#CABFAB",
+      padding: 4,
+      height: 12,
+      width: 12,
+      borderRadius: 6,
+      marginTop: 3,
+      marginRight: 20
+  }
+});
 
   export default styles;
