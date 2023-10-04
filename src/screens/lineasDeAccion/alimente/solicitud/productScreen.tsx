08d81cdbb253960a3a777ae19ba58868a9444307
Bryan Gonzalez <72216201+BryanGonA@users.noreply.github.com>
2023-10-04 13:41:58 -0500

    Update Product List

--- src/screens/lineasDeAccion/alimente/solicitud/productScreen.tsx
+++ src/screens/lineasDeAccion/alimente/solicitud/productScreen.tsx
@@ -1,111 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { FlatList } from 'native-base';
-import { Ionicons } from "@expo/vector-icons";
-import API_BASE_URL from '../../../../components/config/ApiConfig';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, FlatList, Button, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import API_BASE_URL from "../../../../components/config/ApiConfig";
+import axios from "axios";
 
-const logo = require( "../../../../assets/logo/log_blanck.png");
+import styles from "./styles";
 
-import styles from './styles';
-import axios from 'axios';
-interface Product {
-  id: number;
-  name: string;
-  image: string; // Agrega la ruta de la imagen para cada producto
-  availableQuantity: number; // Agrega la cantidad disponible
-}
+const ProductCard = ({ item}) => {
+  return (
+    <View style={styles.productCard}>
+      <Image source={{ uri: item.image }} style={styles.productImage} />
+      <View style={styles.productInfo}>
+        <Text style={styles.productName}>{item.name}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
+        <Text style={styles.productPrice}>${item.quntity}</Text>
+      </View>
+    </View>
+  );
+};
 
+const ProductScreen = () => {
+  const [products, setProducts] = useState();
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
-
-const ProductScreen: React.FC = () => {
-
-
-  const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
-  const [products, setProducts] = useState<Product[]>([]);
-  const navigation = useNavigation();
-
-  const toggleProductSelection = (productId: number) => {
-    setSelectedProducts((prevSelectedProducts) => {
-      if (prevSelectedProducts.includes(productId)) {
-        return prevSelectedProducts.filter((id) => id !== productId);
-      } else {
-        return [...prevSelectedProducts, productId];
-      }
-    });
-  };
-
- const fetchProducts = async () => {
+  
+  const fetchProducts = async () => {
     try {
-      const response = await axios.get(API_BASE_URL + '/products');
+      const response = await axios.get(API_BASE_URL + '/items');
       setProducts(response.data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  const handleCheckout = () => {
-    // Crear el JSON de detalles del carrito a partir de los productos seleccionados
-    const detailsCart = selectedProducts.map((productId) => ({
-      itemId: productId,
-      cafeteriaId: 1,
-    }));
+  const handleSelectProduct = (product) => {
+    const itemId = product.id;
 
-    // Enviar el JSON a la API o realizar la navegación a la pantalla de solicitud
-    console.log(JSON.stringify({ detailsCart }));
+    // Creamos un nuevo objeto JSON para el producto
+    const productJson = {
+      itemId,
+    };
 
-    // Navegar a la pantalla de solicitud (ajusta el nombre de la pantalla según tu configuración)
-    navigation.navigate('Solicitud');
+    setSelectedProducts([...selectedProducts, productJson]);
   };
 
-  const renderItem = ({ item }: { item: Product }) => (
-    
-      <TouchableOpacity
-        onPress={() => toggleProductSelection(item.id)}
-        style={styles.productCard}
-      >
-        <Image source={{ uri: item.image }} style={styles.productImage} />
-        <Text style={styles.productName}>{item.name}</Text>
-        <Text style={styles.productQuantity}>Disponibles: {item.availableQuantity}</Text>
-        {selectedProducts.includes(item.id) && (
-          <Icon name="check-circle" size={24} color="green" style={styles.productCheckIcon} />
-        )}
-      </TouchableOpacity>
-    
+  const renderProductItem = ({ item }) => (
+    <ProductCard item={item}/>
   );
+
+  const onPressContinueButton = () => {
+
+
+    // Enviamos el JSON al servidor
+    axios.post(API_BASE_URL + "/request/create", selectedProducts)
+      .then((response) => {
+        // ...
+      })
+      .catch((error) => {
+        // ...
+      });
+  };
 
   return (
     <View style={styles.container}>
-        <View style={styles.titleBar}>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-              <Ionicons name="ios-arrow-back" size={30} color="#52575D" style={styles.arrow}/>
-            </TouchableOpacity>
-            <Image source={logo} style={styles.logos} />
-            <TouchableOpacity onPress={handleCheckout}>
-              <Icon name="shopping-cart" size={24} color="black" />
-            </TouchableOpacity>
-        </View>
-
-        <View style={styles.container1}>
-            <FlatList
-              data={products}
-              keyExtractor={(item) => item.id.toString()}
-              renderItem={renderItem}
-              numColumns={2}
-            />
-        </View>
-        <TouchableOpacity
-          onPress={handleCheckout}
-          style={styles.fab}
-        >
-          <Text style={{ color: 'white', fontSize: 18 }}>Realizar solicitud</Text>
-        </TouchableOpacity>
+      <FlatList
+        data={products}
+        style={styles.productList}
+        renderItem={renderProductItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 100 }}
+      />
+      <TouchableOpacity style={styles.continueButton}>
+        <Text style={styles.continueButtonText}>Continue</Text>
+      </TouchableOpacity>
     </View>
-    
   );
 };
 
--- src/screens/lineasDeAccion/alimente/solicitud/styles.tsx
+++ src/screens/lineasDeAccion/alimente/solicitud/styles.tsx
@@ -1,148 +1,95 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
-// screen sizing
-const { width, height } = Dimensions.get('window');
-// orientation must fixed
-const SCREEN_WIDTH = width < height ? width : height;
-
-const recipeNumColums = 2;
-// item size
-const RECIPE_ITEM_HEIGHT = 150;
-const RECIPE_ITEM_MARGIN = 20;
 
 // 2 photos per width
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    backgroundColor: '#fff',
-    height: '100%',
-    width: '100%',
-  },
-  container1: {
-    justifyContent:'space-around',
-    padding : 10,
-  },
-  photo: {
-    width: (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) / recipeNumColums,
-    height: RECIPE_ITEM_HEIGHT,
-    borderRadius: 15,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-    justifyContent: 'center',
-  },
-  title: {
-    
-    fontSize: 17,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#444444',
-    marginTop: 3,
-    marginRight: 5,
-    marginLeft: 5,
-  },
-  category: {
-    marginTop: 5,
-    marginBottom: 5
-  },
-
-  
-  header: {
-    backgroundColor: '#57D400',
-    padding: 20,
-    alignItems: 'center',
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-    justifyContent: 'center',
-    elevation: 10,
-    width: '100%',
-  },
-  headerTitle: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  titleBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignContent: "center",
-    alignItems: "center",
-    alignSelf: 'stretch',
-    paddingHorizontal: 16,
-    paddingTop: 30,
-    marginBottom: 10,
-},
-arrow: {
-  width: 30, // Tamaño de la flecha
-  height: 30,
-},
-logos: {
-  width: 40,
-  height: 40,
-  resizeMode: "contain",
-},
-  content: {
-    padding: 20,
-  },
-  description: {
-    fontSize: 16,
-    marginBottom: 20,
-    textAlign: 'justify',
+    flex: 1,
+    backgroundColor: '#f7f7f7',
+    paddingTop:40,
   },
   productList: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding : 10,
+    flex: 1,
+    paddingTop: 16,
   },
   productCard: {
-    width: '48%',
-    aspectRatio: 1,
-    marginBottom: 15,
-    borderRadius: 10,
-    borderColor: '#bdc3c7',
-    borderWidth: 1,
-    margin: 5,
-    padding: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    padding: 16,
+    marginBottom: 16,
   },
   productImage: {
-    width: '100%',
-    height: '70%',
-    resizeMode: 'cover',
-    borderRadius: 8,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 16,
+  },
+  productInfo: {
+    flex: 1,
+    marginRight: 16,
   },
   productName: {
-    marginTop: 5,
-    fontSize: 14,
+    fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center',
+    marginBottom: 4,
   },
-  productQuantity: {
+  productDescription: {
     fontSize: 14,
-    color: 'gray',
+    color: '#666',
+    marginBottom: 4,
   },
-  productCheckIcon: {
-    marginTop: 8,
+  productPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#4caf50',
   },
-  fab: {
-    backgroundColor: '#3498db',
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    width: 150,
-    height: 50,
-    borderRadius: 5,
+  productPriceText: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    color: '#666',
+  },
+  productAmount: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 8,
-
   },
-
-  TextBt: {
+  amountButton: {
+    width: 30,
+    height: 30,
+    backgroundColor: '#ffa726',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  amountButtonText: {
     color: '#fff',
-    fontSize: 15,
+    fontSize: 18,
+  },
+  amountText: {
+    fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center',
+    marginHorizontal: 16,
+  },
+  continueButton: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+    right: 16,
+    backgroundColor: '#4caf50',
+    borderRadius: 8,
+    padding: 16,
+    alignItems: 'center',
+  },
+  continueButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
